e83c543e73390dd4e9c35010d7c1cdccd33088c4
Richard <olella93@gmail.com>
2025-04-09 18:58:43 +0300

    test done

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+      <p>This is the About component.</p>
+    </div>
+  );
+}
+      <About />
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
